fa9005fb252fa030ad69834f14f3305d57f29ed4
Irshad Hussain <razairshad04@gmail.com>
2023-09-12 20:24:55 +0530

    Initial Commit

---
+++ app/page.js
@@ -0,0 +1,81 @@
+"use client"
+import React, { useState } from 'react'
+
+const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [userData,setUserData] = useState({
+    title:"",
+    desc:""
+  })
+  const [mainTask, setmainTask] = useState([])
+// form submit handle
+  const submitHandler = (e)=>{
+     e.preventDefault()
+     setmainTask([...mainTask, {title, desc}])
+     settitle("")
+     setdesc("")
+     console.log(mainTask)
+  }
+
+  // delete operation handle
+  const deleteHandler = (i)=>{
+   let copyTask = [...mainTask]
+   copyTask.splice(i,1)
+   setmainTask(copyTask)
+  }
+
+  let renderTask = <h2>No Task Available </h2>
+
+  if(mainTask.length>0){
+    renderTask = mainTask.map((t,i)=>{
+      return <li key={i} className='flex items-center justify-between mb-8'>
+       <div className='flex justify-between items-center b-5 w-2/3'>
+       <h5 className='text-2xl font-semibold'>{t.title}</h5>
+       <h6 className='text-lg font-medium '>{t.desc}</h6>
+      </div>
+      <button
+       onClick={
+        ()=>{
+        {deleteHandler(i)}
+       }}
+       className='bg-red-400 text-white px-4 py-2 rounded font-bold mb-5'>Delete</button>
+      </li>
+     })
+  }
+  return (
+    <>
+    <h1 className="bg-pink-400 text-white p-5 text-5xl font-bold text-center  ">Irshad's Todo List </h1> 
+    <form onSubmit={submitHandler}>
+     <input 
+        type="text"
+        className="text-2xl border-zinc-800 border-4 m-5 px-4 py-2 rounded"
+        placeholder='Enter title here'
+        value={title}
+        onChange={(e)=>{
+           settitle(e.target.value)
+        }}
+     /> 
+     <input 
+        type="text"
+        className="text-2xl border-zinc-800 border-4 m-5 px-4 py-2 rounded"
+        placeholder='Enter description here'
+        value={desc}
+        onChange={(e)=>{
+          setdesc(e.target.value)
+        }}
+     />  
+    <button className='bg-green-400 text-white rounded px-4 py-2 text-2xl font-bold m-5'>Add Here</button>
+    </form>
+    <hr />
+    <div className='bg-slate-100 p-8'>
+    
+      <ul>
+        {renderTask}
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default page
